5805c8c3b35e93580ea8672f369cb8a86bb45c2e
Phoebe Wang <yifeiwang0613@gmail.com>
2025-10-18 13:15:25 -0500

    sdgd

--- gov-ease/vite.config.js
+++ gov-ease/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Export config
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Chrome will load files from here
+  }
 })
+
